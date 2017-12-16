bc493a3c47da88de0596402173457cb6aa6dc5f4
Neel Indap <indap.n@husky.neu.edu>
2017-12-16 20:50:19 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,209 @@
+var app = require('./AppId.json')
+var request = require("request")
+
+// Route the incoming request based on type (LaunchRequest, IntentRequest,
+// etc.) The JSON body of the request is provided in the event parameter.
+exports.handler = function (event, context) {
+    try {
+        console.log("event.session.application.applicationId=" + event.session.application.applicationId);
+
+        /**
+         * Uncomment this if statement and populate with your skill's application ID to
+         * prevent someone else from configuring a skill that sends requests to this function.
+         */
+
+        if (event.session.application.applicationId !== app.id) {
+            context.fail("Invalid Application ID");
+        }
+
+        if (event.session.new) {
+            onSessionStarted({ requestId: event.request.requestId }, event.session);
+        }
+
+        if (event.request.type === "LaunchRequest") {
+            onLaunch(event.request,
+                event.session,
+                function callback(sessionAttributes, speechletResponse) {
+                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === "IntentRequest") {
+            onIntent(event.request,
+                event.session,
+                function callback(sessionAttributes, speechletResponse) {
+                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === "SessionEndedRequest") {
+            onSessionEnded(event.request, event.session);
+            context.succeed();
+        }
+    } catch (e) {
+        context.fail("Exception: " + e);
+    }
+};
+
+const JOKE_URL = "https://icanhazdadjoke.com/"
+
+/**
+ * Called when the session starts.
+ */
+function onSessionStarted(sessionStartedRequest, session) {
+    // add any session init logic here
+}
+
+/**
+ * Called when the user invokes the skill without specifying what they want.
+ */
+function onLaunch(launchRequest, session, callback) {
+    getWelcomeResponse(callback)
+}
+
+/**
+ * Called when the user specifies an intent for this skill.
+ */
+function onIntent(intentRequest, session, callback) {
+
+    var intent = intentRequest.intent
+    var intentName = intentRequest.intent.name;
+
+    // dispatch custom intents to handlers here
+    if (intentName == "DadJokesIntent") {
+        handleJokeResponse(intent, session, callback)
+    } else if (intentName == "AMAZON.YesIntent") {
+        handleYesResponse(intent, session, callback)
+    } else if (intentName == "AMAZON.NoIntent") {
+        handleNoResponse(intent, session, callback)
+    } else if (intentName == "AMAZON.HelpIntent") {
+        handleGetHelpRequest(intent, session, callback)
+    } else if (intentName == "AMAZON.StopIntent") {
+        handleFinishSessionRequest(intent, session, callback)
+    } else if (intentName == "AMAZON.CancelIntent") {
+        handleFinishSessionRequest(intent, session, callback)
+    } else {
+        throw "Invalid intent"
+    }
+}
+
+
+function handleJokeResponse(intent, session, callback) {
+
+    var speechOutput = "Couldn't fetch a joke"
+
+    getJSON(function (data) {
+        if (data != "ERROR") {
+            var speechOutput = data
+        }
+        callback(session.attributes, buildSpeechletResponseWithoutCard(speechOutput, "", false))
+    })
+}
+
+function handleYesResponse(intent, session, callback) {
+    handleJokeResponse(intent, session, callback)
+}
+
+function handleNoResponse(intent, session, callback) {
+}
+
+
+function getJSON(callback) {
+    var options = {
+        url: JOKE_URL,
+        headers: {
+            'Accept': 'application/json'
+        }
+    };
+    request.get(options, function (error, response, body) {
+        let data = JSON.parse(body)
+        let joke = data.joke
+        callback(joke)
+    })
+}
+
+/**
+ * Called when the user ends the session.
+ * Is not called when the skill returns shouldEndSession=true.
+ */
+function onSessionEnded(sessionEndedRequest, session) {
+
+}
+
+// ------- Skill specific logic -------
+
+function getWelcomeResponse(callback) {
+
+    var speechOutput = 'Welcome to dad jokes!'
+
+    var reprompt = "Ask me for a dad joke";
+
+    var header = "Dad Jokes!"
+
+    var shouldEndSession = false
+
+    var sessionAttributes = {
+        "speechOutput": speechOutput,
+        "repromptText": reprompt
+    }
+
+    callback(sessionAttributes, buildSpeechletResponse(header, speechOutput, reprompt, shouldEndSession))
+}
+
+function handleGetHelpRequest(intent, session, callback) {
+    // Ensure that session.attributes has been initialized
+    if (!session.attributes) {
+        session.attributes = {};
+    }
+}
+
+function handleFinishSessionRequest(intent, session, callback) {
+    // End the session with a "Good bye!" if the user wants to quit the game
+    callback(session.attributes,
+        buildSpeechletResponseWithoutCard("Good bye!", "", true));
+}
+
+
+// ------- Helper functions to build responses for Alexa -------
+
+
+function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
+    return {
+        outputSpeech: {
+            type: "PlainText",
+            text: output
+        },
+        card: {
+            type: "Simple",
+            title: title,
+            content: output
+        },
+        reprompt: {
+            outputSpeech: {
+                type: "PlainText",
+                text: repromptText
+            }
+        },
+        shouldEndSession: shouldEndSession
+    };
+}
+
+function buildSpeechletResponseWithoutCard(output, repromptText, shouldEndSession) {
+    return {
+        outputSpeech: {
+            type: "PlainText",
+            text: output
+        },
+        reprompt: {
+            outputSpeech: {
+                type: "PlainText",
+                text: repromptText
+            }
+        },
+        shouldEndSession: shouldEndSession
+    };
+}
+
+function buildResponse(sessionAttributes, speechletResponse) {
+    return {
+        version: "1.0",
+        sessionAttributes: sessionAttributes,
+        response: speechletResponse
+    };
+}
